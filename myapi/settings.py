938916450f1b1af148afbdd74f627955b318907c
koabc9999 <koabc9999@gmail.com>
2021-06-10 13:45:28 +0900

    Ready for deploy

--- myapi/settings.py
+++ myapi/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].upgrade(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
